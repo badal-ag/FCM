4844ef7df003e0a3faf1c1a66584b288f45797f3
Badal Agarwal <63859255+badal-ag@users.noreply.github.com>
2022-07-16 10:40:29 +0530

    Personal Configs Removed

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js");
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDDQQ7yrte7jiqIcCLvKnqzsafEfdqhD2A",
-    authDomain: "tutorhuntz-testing.firebaseapp.com",
-    projectId: "tutorhuntz-testing",
-    storageBucket: "tutorhuntz-testing.appspot.com",
-    messagingSenderId: "712233401124",
-    appId: "1:712233401124:web:0c231b0d7cf99a827711b1",
-    measurementId: "G-3KNJCL6R9L"
+    /* Personal Configs */
 };
 firebase.initializeApp(firebaseConfig);
 
