a674bd7da79cc40d18c0a707ed2bf1689bc581c3
Badal Agarwal <63859255+badal-ag@users.noreply.github.com>
2022-07-16 10:39:18 +0530

    Personal Configurations Removed

--- public/app.js
+++ public/app.js
@@ -1,11 +1,5 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDDQQ7yrte7jiqIcCLvKnqzsafEfdqhD2A",
-    authDomain: "tutorhuntz-testing.firebaseapp.com",
-    projectId: "tutorhuntz-testing",
-    storageBucket: "tutorhuntz-testing.appspot.com",
-    messagingSenderId: "712233401124",
-    appId: "1:712233401124:web:0c231b0d7cf99a827711b1",
-    measurementId: "G-3KNJCL6R9L"
+    /* Your Personal Configurations */
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -20,7 +14,7 @@ function allowNotification() {
         if (permission === 'granted') {
             console.log('Permission');
 
-            messaging.getToken({ vapidKey: 'BLyqPjt8wGKV1HWPFXiFuZnH7iRumhQUcPMCHQDviXs86uBw4kRzA0XfQu1BSV7ZMMz4Sxt_es0BmmGLIeAJCew' }).then((currentToken) => {
+            messaging.getToken({ vapidKey: 'YOUR_VAPID_KEY' }).then((currentToken) => {
                 if (currentToken) {
                     console.log("Token Received")
                     console.log(currentToken)
